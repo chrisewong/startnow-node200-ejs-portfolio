50e631f24e4840169db0029b54cb719fcb7979bd
Chris <chrisewong@gmail.com>
2018-03-28 20:48:21 -0700

    Initial push

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const profile = require('./profile');
 const path = require('path');
+const sgMail = require('@sendgrid/mail');
+
 
 
 const app = express();
@@ -38,14 +40,38 @@ app.get('/', (req, res) => {
   });
   
   app.post('/thanks', (req, res) => {
-    res.render('thanks', { contact: req.body })
+    console.log('email: ' + req.body.email);
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+        to: 'chrisewong@gmail.com',
+        from: req.body.email,
+        subject: 'Sending with SendGrid is Fun CONTACT',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+        };
+    sgMail.send(msg, function(errSend, dataSend){
+      if(errSend) {
+        console.log('errSend: ' + errSend);
+        res.render('thanks', { contact: req.body });
+        return;
+      }
+      if (dataSend[0].statusCode == 401) {
+        console.log('Sendgrid indicated unauthorized: ' + JSON.stringify(dataSend));
+        res.render('thanks', { contact: req.body });
+        return;
+      }
+      console.log(JSON.stringify(dataSend));
+      res.render('thanks', { contact: req.body })
+    });
+//    console.log('req.body.firstName: ' + req.body.firstName);
+//    res.render('thanks', { contact: req.body })
   });
 
   // Notice now the data is the second argument passed to the template render method
 
 
 
-
-app.listen(8080, () => {
+var PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
   console.log('listening at http://localhost:8080');
 })
